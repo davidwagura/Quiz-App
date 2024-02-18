8aefead85f081cd369c1f81eb68184a7801b4e42
David wanjohi <davidwagura83@gmail.com>
2024-02-18 19:33:43 +0300

    create constants

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,90 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
+  import { ref, computed } from 'vue'
+
+  const questions = ref([
+    {
+      question: 'what is vue JS?',
+      answer: 0,
+      options: [
+        'A front end framework',
+        'A library',
+        'An ice cream maker'
+      ],
+      selected: null
+    },
+    {
+      question: 'what is vueX?',
+      answer: 2,
+      options: [
+        'Vue with an x',
+        'A cheese selection',
+        'State management library'
+      ],
+      selected: null
+    },
+    {
+      question: 'what is Vue Router used for?',
+      answer: 1,
+      options: [
+        'A routing libraly for vue JS',
+        'Walking in space',
+        'Burger sauce',
+        'Quizes'
+      ],
+      selected: null
+    },
+
+
+  ])
+
+  const quizCompleted = ref (false)
+  const currentQuestion =  ref(0)
+  const score = computed(() => {
+    let value = 0
+    questions.value.map(q => {
+      if (q.selected == q.answer) {
+        value++
+      }
+    })
+    return value
+  })
+  
+  const getCurrentQuestion = computed (() => {
+    let question = questions.value[currentQuestion.value]
+    question.index = currentQuestion.value
+    return question
+  })
+  
+  const SetAnswer = evt => {
+    questions.value[currentQuestion.value].selected = evt.target.value
+    evt.target.value = null
+  }
+
+const NextQuestion = () => {
+  if(currentQuestion.value < questions.value.length - 1) {
+    currentQuestion.value++
+  }
+  else {
+    quizCompleted.value = true
+  }
+}
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>Hello world</h1>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'montserat', sans-serif;
+  }
+  body {
+    background-color: rgb(50, 1, 50);
+    color: #FFF;
+  }
 </style>
